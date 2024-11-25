d604a20a65772f68e315f47d934c0e472a46a67c
shaijuabraham <Shaijuabraham98@gmail.com>
2024-11-25 15:14:55 -0500

    Make Schedue is Workiing

--- Finial_Project_RealEstateWebPage/Controllers/ViewHomeController.cs
+++ Finial_Project_RealEstateWebPage/Controllers/ViewHomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Finial_Project_RealEstateWebPage.Models;
 using Finial_Project_RealEstateWebPage.Models.associateclass;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
 
 namespace Finial_Project_RealEstateWebPage.Controllers
 {
@@ -20,27 +21,15 @@ namespace Finial_Project_RealEstateWebPage.Controllers
             AgentInfo agentInfo = agent.AgentContactInfo(id);
             AgentCompanyInfo agentCompany = agentCompanyInfo.RelatorCompanyInfo(id);
 
-
             if (home != null)
             {
-                /*if (home.HomeAmenities != null && home.HomeAmenities.SelectedAmenities.Count > 0)
-                {
-                     string.Join(", ", home.HomeAmenities.SelectedAmenities);
-                }
-                if (home.HomeUtility != null && home.HomeUtility.SelectedUtility.Count > 0)
-                {
-                    string.Join(", ", home.HomeUtility.SelectedUtility);
-                }*/
-
-                // Create and populate view model
-                var viewModel = new PropertyDetails
+                // Create and populate view model 
+                PropertyDetails viewModel = new PropertyDetails
                 {
                     HomeInfo = home,
                     AgentInfo = agentInfo,
-                    AgentCompanyInfo = agentCompany,
+                    AgentCompanyInfo = agentCompany
                 };
-
-
                 return View("~/Views/Home/ViewHome.cshtml", viewModel);
             }
             else
@@ -51,11 +40,38 @@ namespace Finial_Project_RealEstateWebPage.Controllers
             return View("~/Views/Home/HomePage.cshtml");
         }
 
-/*this is the For teh home Shoing NAv Bar */
+        /*This will rediret to the Home Showing Page in the nav_bar*/
         public IActionResult SechduleHomeShowing(string id)
         {
-            
+            if (id != null)
+            {
+                Console.WriteLine($"{id}");
+                var options = new CookieOptions { Expires = DateTime.Now.AddMinutes(10) };
+                Response.Cookies.Append("PropertyID", id, options);
+                return View("~/Views/Home/HomeShowingPage.cshtml");
+            }
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SechduleHomeShowingReq(string FullName,string PhoneNumber,
+                    string Email, string Date, string time)
+        {
+            string Id = Request.Cookies["PropertyID"];
+            Console.WriteLine(FullName, PhoneNumber, Email, Date, time);
+            Console.WriteLine(Id);
+            Console.WriteLine("startiing RequestPropertyShowing");
+            if (Id != null) 
+            {
+                PropertyUserRequest propertyUserRequest = new PropertyUserRequest();
+                propertyUserRequest.RequestPropertyShowing(Id,FullName,PhoneNumber,Email,Date,time);
+                Response.Cookies.Delete("PropertyID");
+                Console.WriteLine("Cookie deleted.");
+               // return Re("ViewHome","SechduleHomeShowing");
+            }
+
+            return View("~/Views/Home/HomeShowingPage.cshtml");
+        }
+
     }
 }
