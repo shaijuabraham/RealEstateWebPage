1e7bba84ef52b2059d4757efea4e5093735776e5
Ankit9098 <ankit89863@gmail.com>
2024-11-24 21:24:42 -0500

    Added Search

--- Finial_Project_RealEstateWebPage/Controllers/HomeController.cs
+++ Finial_Project_RealEstateWebPage/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace Finial_Project_RealEstateWebPage.Controllers
     {
         // Checkig if the comit works
 
+
+
         [HttpGet]
         public IActionResult HomePageRun()
         {
@@ -75,6 +77,85 @@ namespace Finial_Project_RealEstateWebPage.Controllers
             return View("~/Views/Home/HomePage.cshtml");
 
         }
+
+        [HttpPost]
+        public IActionResult SearchHomes(string search, string propertyType)
+        {
+            List<Home> homes = new List<Home>();
+
+            try
+            {
+                DBConnect objDB = new DBConnect();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "SearchPropertyList";
+
+                // Add parameters based on the search input
+                if (!string.IsNullOrEmpty(search))
+                {
+                    int zipCode;
+                    if (int.TryParse(search, out zipCode))
+                    {
+                        command.Parameters.Add(new SqlParameter("@ZipCode", zipCode));
+                    }
+                    else
+                    {
+                        command.Parameters.Add(new SqlParameter("@City", search));
+                        command.Parameters.Add(new SqlParameter("@State", search));
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(propertyType))
+                {
+                    command.Parameters.Add(new SqlParameter("@PropertyType", propertyType));
+                }
+
+                DataSet ds = objDB.GetDataSetUsingCmdObj(command);
+
+                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow record in ds.Tables[0].Rows)
+                    {
+                        Home home = new Home();
+
+                        if (!record.IsNull("AskingPrice"))
+                            home.AskingPrice = Convert.ToDouble(record["AskingPrice"]);
+
+                        if (!record.IsNull("BedRooms"))
+                            home.BedRooms = Convert.ToInt32(record["BedRooms"]);
+
+                        if (!record.IsNull("Bathrooms"))
+                            home.BathRooms = Convert.ToInt32(record["Bathrooms"]);
+
+                        if (!record.IsNull("HomeSize"))
+                            home.HomeSize = record["HomeSize"].ToString();
+
+                        if (!record.IsNull("Street"))
+                            home.Street = record["Street"].ToString();
+
+                        if (!record.IsNull("City"))
+                            home.City = record["City"].ToString();
+
+                        if (!record.IsNull("State"))
+                            home.State = record["State"].ToString();
+
+                        if (!record.IsNull("ZipCode"))
+                            home.ZipCode = Convert.ToInt32(record["ZipCode"]);
+
+                        homes.Add(home);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error during search: " + ex.Message);
+            }
+
+            ViewBag.HomesList = homes;
+            return View("~/Views/Search/SearchResults.cshtml");
+        }
+
+
     }
 
 }
