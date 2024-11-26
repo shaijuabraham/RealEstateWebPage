666f53dc26c9a2ea3fcfd59ffbec39f1acb4ace1
shaijuabraham <Shaijuabraham98@gmail.com>
2024-11-26 18:14:52 -0500

    Make Offer is done

--- Finial_Project_RealEstateWebPage/Controllers/PropertyOfferController.cs
+++ Finial_Project_RealEstateWebPage/Controllers/PropertyOfferController.cs
@@ -1,6 +1,7 @@
 ﻿using Finial_Project_RealEstateWebPage.Models;
 using Finial_Project_RealEstateWebPage.Models.associateclass;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.FlowAnalysis;
 
 namespace Finial_Project_RealEstateWebPage.Controllers
 {
@@ -37,29 +38,20 @@ namespace Finial_Project_RealEstateWebPage.Controllers
         }
 
         [HttpPost]
-        public IActionResult MakePropertyOfferReq(OfferClass offerClass)
+        public IActionResult MakePropertyOfferReq(string FullName, string BuyerPhone,
+                                                string BuyerEmail, string OfferAmount, string SaleType,
+                                                string Contingencies, string NeedToSell, string MoveInDate)
         {
 
             string propertyID = Request.Cookies["SelectedPropertyID"];
 
-            string fullName = offerClass.FullName;
-            string buyerPhone = offerClass.BuyerPhone;
-            string email = offerClass.BuyerEmail;
-            string offerAmount = offerClass.OfferAmount;
-            string saleType = offerClass.SaleType;
-            string contingencies = offerClass.Contingencies;
-            string needToSell = offerClass.NeedToSell;
-            string moveInDate = offerClass.MoveInDate;
-
-            Console.WriteLine(propertyID, fullName, buyerPhone, email,
-                                                offerAmount, saleType,
-                                                contingencies, needToSell, moveInDate);
             //if (propertyID != null && offerClass != null)
-            if (ModelState.IsValid)
+            if (propertyID != null)
             {
-                offerClass.MakeOffer(propertyID, fullName, buyerPhone, email,
-                                                offerAmount, saleType,
-                                                contingencies, needToSell, moveInDate);
+                OfferClass offerClass = new OfferClass();
+                offerClass.MakeOffer(propertyID, FullName, BuyerPhone, BuyerEmail,
+                                                OfferAmount, SaleType,
+                                                Contingencies, NeedToSell, MoveInDate);
                 return RedirectToAction("ViewPropertyInfo", "ViewHome", new { id = propertyID });
             }
             else
