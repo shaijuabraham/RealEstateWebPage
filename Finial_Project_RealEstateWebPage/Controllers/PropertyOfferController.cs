88fd9894b64758c450086d853b9e3c2bab3ee56b
shaijuabraham <Shaijuabraham98@gmail.com>
2024-11-26 12:22:34 -0500

    Made the Changes in the Offere For user and agent

---
+++ Finial_Project_RealEstateWebPage/Controllers/PropertyOfferController.cs
@@ -0,0 +1,34 @@
+﻿using Finial_Project_RealEstateWebPage.Models;
+using Finial_Project_RealEstateWebPage.Models.associateclass;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finial_Project_RealEstateWebPage.Controllers
+{
+    public class PropertyOfferController : Controller
+    {
+        public IActionResult AgentViewOffer()
+        {
+            Console.WriteLine("Agent offer Showing function Running .");
+            string userId = Request.Cookies["UserID"];
+            OfferClass home = new OfferClass();
+            List<OfferClass> homes = home.GetPropertyOffer(userId);
+            if (homes != null && homes.Count > 0)
+            {
+                ViewBag.RelatorProertyOffers = homes;
+            }
+            else
+            {
+                ViewBag.RelatorProertyOffers = new List<Home>();  //empty list to avoid null reference
+            }
+
+            return View("~/Views/RealtorPage/AgentOfferShowing.cshtml");
+        }
+
+
+        [HttpPost]
+        public IActionResult MakePropertyOffer()
+        {
+            return View();  
+        }
+    }
+}
--- Finial_Project_RealEstateWebPage/Models/associateclass/OfferClass.cs
+++ Finial_Project_RealEstateWebPage/Models/associateclass/OfferClass.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
 
-namespace FunctionClassLibrary.AssociateClass
+namespace Finial_Project_RealEstateWebPage.Models.associateclass
 {
     public class OfferClass
     {
@@ -16,8 +19,12 @@ namespace FunctionClassLibrary.AssociateClass
         public string FullName { get; set; }
         public string BuyerPhone { get; set; }
         public string BuyerEmail { get; set; }
+        public string PropertyID { get; set; }
+        public string OfferId { get; set; }
 
 
+
+        public OfferClass() { }//empty Constructor
         public OfferClass(string offerAmount, string saleType, string contingencies, string needToSell, 
                            string moveInDate, string fullName, string buyerPhone, string buyerEmail)
         {
@@ -29,6 +36,53 @@ namespace FunctionClassLibrary.AssociateClass
             FullName = fullName;
             BuyerPhone = buyerPhone;
             BuyerEmail = buyerEmail;
+        }
+
+        /*Get the list of property offcer baseed on the agentID*/
+
+        public List<OfferClass> GetPropertyOffer(string userId)
+        {
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+            Console.WriteLine("Available in the result set.1");
+            List<OfferClass> homes = new List<OfferClass>();
+            try
+            {
+
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "GetPropertyOffer";
+                objCommand.Parameters.Clear();
+                objCommand.Parameters.AddWithValue("@AgentID", userId);
+                DataSet ds = objDB.GetDataSetUsingCmdObj(objCommand);
+
+                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow propertyRow in ds.Tables[0].Rows)
+                    {
+                        OfferClass home = new OfferClass();
+
+                        if (propertyRow.Table.Rows.Count > 0)
+                        {
+                            home.PropertyID = propertyRow["PropertyId"].ToString();
+                            home.OfferId = propertyRow["id"].ToString();
+                        }
+
+                        homes.Add(home);
+                        Console.WriteLine("Available in the result set.2");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data available in the result set.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error retrieving data: " + ex.Message);
+            }
+
+            return homes;
+
         }
     }
 }
