edf7f354d87b5f15e561b3f26c2d00db3047ab24
shaijuabraham <Shaijuabraham98@gmail.com>
2024-11-26 20:27:22 -0500

    Add the review

--- Finial_Project_RealEstateWebPage/Models/associateclass/OfferClass.cs
+++ Finial_Project_RealEstateWebPage/Models/associateclass/OfferClass.cs
@@ -15,7 +15,7 @@ namespace Finial_Project_RealEstateWebPage.Models.associateclass
         public string OfferAmount { get; set; }
         public string SaleType { get; set; }
         public string Contingencies { get; set; }
-        public string NeedToSell { get; set; }
+        public string NeedToSell { get; set; } 
         public string MoveInDate { get; set; }
         public string FullName { get; set; }
         public string BuyerPhone { get; set; }
