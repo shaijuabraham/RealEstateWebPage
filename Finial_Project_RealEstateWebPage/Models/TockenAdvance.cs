91fb5a06f3c5cd86b0a4f40cc90de67c61ef2c89
shaijuabraham <Shaijuabraham98@gmail.com>
2024-12-05 15:13:18 -0500

    ser

---
+++ Finial_Project_RealEstateWebPage/Models/TockenAdvance.cs
@@ -0,0 +1,42 @@
+﻿using Mono.TextTemplating;
+using System.Data.Common;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection.Emit;
+using System.Text.Json.Serialization;
+using Utilities;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Finial_Project_RealEstateWebPage.Models
+{
+
+    [Serializable]
+    public class TockenAdvance
+    {
+
+        public string AccountNumber { get; set; }
+
+        public string RoutingNumber { get; set; }
+
+        public float Amount { get; set; }
+
+
+        public void SentTockenAmountForSelectProperty(string propertyId,float amount, string accountNumber, string routingNumber)
+        {
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "InsertTockenAmountInfo"; // Stored procedure name
+            objCommand.Parameters.Clear();
+            objCommand.Parameters.AddWithValue("@propertyID", propertyId);
+            objCommand.Parameters.AddWithValue("@AccountNumber", accountNumber);
+            objCommand.Parameters.AddWithValue("@RoutingNumber", routingNumber);
+            objCommand.Parameters.AddWithValue("@Amount", amount);
+            objDB.DoUpdateUsingCmdObj(objCommand);
+
+
+        }
+    }
+}
